fbef2e5a3b085977821ac7392fcfe138c36f1d63
ProfBlox <39020060+ProfBlox@users.noreply.github.com>
2018-05-20 15:04:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,42 @@
+// DEPENDANCIES
+let Discord = require('discord.js');
+let roblox = require('roblox-js');
+let bot = new Discord.Client();
 
+// LOGIN INFO
+let username = ""; // ROBLOX
+let password = ""; // ROBLOX
+
+// MISC
+let prefix = "!" // Prefix used for the command
+let GroupId = 3944945;
+
+// COMMAND
+
+bot.on("message", async message => { // Event runs when there is a new message
+if(message.author.bot) return; // Here we check if the message sender is the bot, if it is, it returns and does not carry any further.
+if(message.content.indexOf(prefix) !== 0) return; // Checks if the message has the Prefix
+
+// Here we separate our "command" and our "arguments/args" for the command. 
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
+// Checks if the command is matching the provided string
+
+if(command === "shout") {
+    if(!message.member.roles.some(r=>["High Ranks"].includes(r.name)) ) // OPTIONAL - Checks if the sender has the specified roles to carry on further
+        return message.reply("You can't use this command.");
+  roblox.login(username, password)
+  .then(function () {
+    const shoutMSG = args.join(" "); // Joins the arguments minus prefix to form the message to be shouted
+        roblox.shout(GroupId, shoutMSG);
+        console.log(`Shouted ${shoutMSG}`); // OPTIONAL - Logs specified string to the console
+        message.channel.send(`Shouted ${shoutMSG} to the group!`) // OPTIONAL - Sends a message to the channel
+  })
+.catch(function (err) { // Catches any errors with the function
+    console.error(err.stack);
+    });
+  }
+})
+
+bot.login(process.env.token) // Logs into Discord
